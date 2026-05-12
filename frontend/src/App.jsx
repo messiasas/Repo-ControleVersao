346f532453eb601858ff1842d2e3524546dae4f9
messiasas <filipe.silva68@fatec.sp.gov.br>
2026-05-12 18:25:47 -0300

    feat: tabela seleção e personalizada + pagina inicial sem style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
-import Home from "./pages/Home.jsx"
+import PaxTable from "./pages/PaxTable.jsx"
+import Home from "./pages/Home.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App(){
-    return <Home/>
+    return(
+      <BrowserRouter>
+        <Routes>
+          
+          <Route path="/" element={<Home />} />
+
+          <Route path="/equipamentos" element={<PaxTable />} />
+          {/* <Route path="/chaves" element={<PaxTable />} />  */}
+        </Routes>
+      </BrowserRouter>
+      )
 }
 
 export default App;
