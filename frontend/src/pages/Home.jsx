0c6924f6431b2b700f85b48d8fbebbae3d9658be
messiasas <filipe.silva68@fatec.sp.gov.br>
2026-05-13 19:59:11 -0300

    feat: Adição de layout tela inicial

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,99 @@
-// src/pages/Home.jsx
-import PaxTable from "./PaxTable.jsx"
+import "../styles/home.css";
+import Header from "../components/Header.jsx";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>Tela Inicial</h1>
 
-      <button onClick={() => navigate("/equipamentos")}>
-        Ir para Equipamentos
-      </button>
+    
 
-      {/* <button onClick={() => navigate("/chaves")}>
-        Ir para Chaves
-      </button> */}
-      
-       {/* <PaxTable /> */}
+    <div className="home-container">
+
+    <Header />
+
+      {/* Conteúdo principal */}
+      <div className="home-grid">
+
+        {/* Coluna esquerda */}
+        <div className="left-column">
+
+          {/* Busca */}
+          <div className="search-row">
+            <input
+              type="text"
+              placeholder="Busca por modelo"
+              className="search-input"
+            />
+
+            <button className="search-button">
+              🔍
+            </button>
+          </div>
+
+          {/* Terminais */}
+          <div className="gray-card terminals-card">
+            terminais
+          </div>
+
+        </div>
+
+
+        {/* Controle de versões */}
+        <div className="gray-card version-card">
+
+            {/* NOVA DIV DO TÍTULO */}
+          <div className="version-header">
+            <span className="version-title">
+              Versionamentos
+            </span>
+          </div>
+
+          <div className="version-buttons">
+
+            {/* PAX */}
+            <button
+              className="version-button"
+              onClick={() => navigate("/equipamentos")}
+            >
+              PAX
+            </button>
+
+            {/* SUMNI */}
+            <button
+              className="version-button"
+            >
+              SUMNI
+            </button>
+
+          </div>
+        </div>
+
+
+
+
+        
+
+      </div>
+
+      {/* Dashboards inferiores */}
+      <div className="bottom-dashboards">
+
+        <div className="gray-card dashboard-card">
+          dashboard
+        </div>
+
+        <div className="gray-card dashboard-card">
+          dashboard
+        </div>
+
+        <div className="gray-card dashboard-card">
+          dashboard
+        </div>
+
+      </div>
+
     </div>
   );
 }
