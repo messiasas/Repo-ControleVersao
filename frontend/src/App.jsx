e26bd056a37f66438e71b3b239231f6010868d01
messiasas <filipe.silva8@aluno.cps.sp.gov.br>
2026-06-04 20:57:35 -0300

    feat: frontend tela de login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import TransireTable from "./pages/TransireTable.jsx"
 import VersionView from "./pages/VersionView.jsx"
+import Login from "./pages/Login";
+import Admin from "./pages/admin.jsx"
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -9,8 +11,10 @@ function App(){
         <Routes>
 
         <Route path="/" element={<TransireTable/>} />
-        <Route path="/" element={<TransireTable />} />
         <Route path="/version-view" element={<VersionView />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+
 
         </Routes> 
       </BrowserRouter>
--- frontend/src/pages/TransireTable.jsx
+++ frontend/src/pages/TransireTable.jsx
@@ -3,6 +3,7 @@ import {getVersions} from "../services/api.js"
 import Header from "../components/Header.jsx";
 import Filters from "../components/Filters.jsx"
 import Grid from "../components/Grid.jsx"
+import { useNavigate } from "react-router-dom";
 
 
 function TransireTable(){
@@ -27,6 +28,8 @@ function TransireTable(){
 
     console.log(data);
 
+     const navigate = useNavigate();
+
     return (
     <div className="app-container">
 
@@ -38,7 +41,10 @@ function TransireTable(){
           Controle de versão
         </button>
 
-        <button className="nav-button">
+        <button 
+          className="nav-button"
+          onClick={() => navigate("/login")}>
+        
           Suporte
         </button>
 
